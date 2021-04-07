960c3d04f9824adbb8c1aa49fb9bd0a30152dd6b
Cathella <ncathie214@gmail.com>
2021-04-07 08:41:39 +0300

    Use StimulusReflex to remove quotes

---
+++ app/reflexes/quotes_reflex.rb
@@ -0,0 +1,11 @@
+class QuotesReflex < StimulusReflex::Reflex
+  # include  CableReady::Broadcaster
+
+  def destroy
+    id = element.dataset[:id]
+    Quote.find(id).destroy
+    cable_ready['hello_quotes'].remove(
+      selector: "#quote-#{id}"
+    )
+  end
+end
